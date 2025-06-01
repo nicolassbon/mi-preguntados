f8cd247eabdc767a4190d677b954e4e4852e4340
nicolassbon <nicolasbon019@gmail.com>
2025-05-31 22:59:27 -0300

    se agrego la funcionalidad del mapa interactivo en el registro

--- Configuration.php
+++ Configuration.php
@@ -10,6 +10,7 @@ require_once("controller/SongController.php");
 require_once("controller/TourController.php");
 require_once("controller/RegistroController.php");
 require_once("controller/LoginController.php");
+require_once("controller/UbicacionController.php");
 
 
 require_once("model/GroupModel.php");
@@ -17,6 +18,7 @@ require_once("model/SongModel.php");
 require_once("model/TourModel.php");
 require_once("model/RegistroModel.php");
 require_once("model/LoginModel.php");
+require_once("model/UbicacionModel.php");
 
 require_once("model/EmailModel.php");
 require_once("controller/EmailController.php");
@@ -29,89 +31,99 @@ include_once('vendor/mustache/src/Mustache/Autoloader.php');
 
 class Configuration
 {
-    public function getDatabase()
-    {
-        $config = $this->getIniConfig();
+  public function getDatabase()
+  {
+    $config = $this->getIniConfig();
 
-        return new Database(
-            $config["database"]["server"],
-            $config["database"]["user"],
-            $config["database"]["dbname"],
-            $config["database"]["pass"]
-        );
-    }
+    return new Database(
+      $config["database"]["server"],
+      $config["database"]["user"],
+      $config["database"]["dbname"],
+      $config["database"]["pass"]
+    );
+  }
 
-    public function getIniConfig()
-    {
-        return parse_ini_file("configuration/config.ini", true);
-    }
+  public function getIniConfig()
+  {
+    return parse_ini_file("configuration/config.ini", true);
+  }
 
-    public function getRegistroController()
-    {
-        return new RegistroController(
-            new RegistroModel($this->getDatabase()),
-            $this->getViewer()
-        );
-    }
+  public function getRegistroController()
+  {
+    return new RegistroController(
+      new RegistroModel($this->getDatabase()),
+      new UbicacionModel($this->getDatabase()),
+      $this->getViewer()
+    );
+  }
 
-    public function getPerfilController()
-    {
-      return new PerfilController(
-        new PerfilModel($this->getDatabase()),
-        $this->getViewer()
-      );
-    }
+  public function getUbicacionController()
+  {
+    return new UbicacionController(
+      new UbicacionModel($this->getDatabase())
+    );
+  }
 
-    public function getLoginController(){
-        return new LoginController(
-            new LoginModel($this->getDatabase()),
-            $this->getViewer()
-        );
-    }
+  public function getPerfilController()
+  {
+    return new PerfilController(
+      new PerfilModel($this->getDatabase()),
+      $this->getViewer()
+    );
+  }
 
-
-    public function getSongController()
-    {
-        return new SongController(
-            new SongModel($this->getDatabase()),
-            $this->getViewer()
-        );
-    }
-
-    public function getTourController()
-    {
-        return new TourController(
-            new TourModel($this->getDatabase()),
-            $this->getViewer()
-        );
-    }
-
-    public function getHomeController()
-    {
-        return new HomeController($this->getViewer());
-    }
+  public function getLoginController()
+  {
+    return new LoginController(
+      new LoginModel($this->getDatabase()),
+      $this->getViewer()
+    );
+  }
 
 
-    public function getGroupController()
-    {
-        return new GroupController(new GroupModel($this->getDatabase()), $this->getViewer());
-    }
+  public function getSongController()
+  {
+    return new SongController(
+      new SongModel($this->getDatabase()),
+      $this->getViewer()
+    );
+  }
 
-    public function getEmailController(){
-        return new EmailController(
-            new EmailModel($this->getDatabase()),
-            $this->getViewer()
-        );
-    }
+  public function getTourController()
+  {
+    return new TourController(
+      new TourModel($this->getDatabase()),
+      $this->getViewer()
+    );
+  }
 
-    public function getRouter()
-    {
-        return new Router("getHomeController", "show", $this);
-    }
+  public function getHomeController()
+  {
+    return new HomeController($this->getViewer());
+  }
 
-    public function getViewer()
-    {
-        //return new FileView();
-        return new MustachePresenter("view");
-    }
+
+  public function getGroupController()
+  {
+    return new GroupController(new GroupModel($this->getDatabase()), $this->getViewer());
+  }
+
+  public function getEmailController()
+  {
+    return new EmailController(
+      new EmailModel($this->getDatabase()),
+      $this->getViewer()
+    );
+  }
+
+  public function getRouter()
+  {
+    return new Router("getHomeController", "show", $this);
+  }
+
+  public function getViewer()
+  {
+    //return new FileView();
+    return new MustachePresenter("view");
+  }
 }
