6c6878de978961c82b4906aa74393cbb0ec84209
Nicolas <nicolasbon019@gmail.com>
2025-06-14 13:40:00 -0300

    se agrego la dificultad de la pregunta y refactor de funcionalidad jugar

--- controller/PartidaController.php
+++ controller/PartidaController.php
@@ -6,38 +6,76 @@ class PartidaController
     private $model;
     private $view;
 
-    public function __construct($model, $view){
+    public function __construct($model, $view)
+    {
         $this->model = $model;
         $this->view = $view;
     }
 
-
-
-    public function show()
+    public function crearPartida()
     {
-
         $id_usuario = $_SESSION['usuario_id'] ?? null;
+        $_SESSION['puntaje'] = 0;
 
-        if($id_usuario == null){
+        if ($id_usuario == null) {
             header('Location: /inicio/show');
             exit;
         }
 
+        //crear partida
+        $id_partida = $this->model->crearPartida($id_usuario);
+
+        $_SESSION['id_partida'] = $id_partida;
+
+        header('Location: /ruleta/show');
+        exit();
+    }
+
+    public function jugar()
+    {
+
+        $id_usuario = $_SESSION['usuario_id'] ?? null;
+
+        if ($id_usuario == null) {
+            header('Location: /inicio/show');
+            exit;
+        }
+
+        $categoria = $this->model->getCategoriaAleatoria();
+
+        $nombre_categoria = $categoria["nombre"];
+
+        $pregunta = $this->model->obtenerPregunta($id_usuario, $categoria["id_categoria"]);
+
+        $id_pregunta = $pregunta["id_pregunta"];
+
+        $pregunta_texto = $pregunta["pregunta"];
+
+        $respuestas = $this->model->getRespuestasPorIdPreguntaAleatoria($id_pregunta);
+
+        $_SESSION['id_pregunta'] = $id_pregunta;
+        $_SESSION['pregunta'] = $pregunta_texto;
+        $_SESSION['nombre_categoria'] = $nombre_categoria;
+        $_SESSION['opciones'] = $respuestas;
         $_SESSION['inicio_pregunta'] = time();
 
-        $fondo = $this->model->getColorCategoria($_SESSION['nombre_categoria']);
-        $foto = $this->model->getFotoCategoria($_SESSION['nombre_categoria']);
+        $fondo = $this->model->getColorCategoria($nombre_categoria);
+        $foto = $this->model->getFotoCategoria($nombre_categoria);
         $user = $this->model->getUsuario($id_usuario);
 
         $tiempo_restante = $this->model->getTiempo();
+
+        // Se le entrego la pregunta, actualizar datos bdd
+        $this->model->incrementoDeEntregadas($id_usuario);
+        $this->model->marcarPreguntaComoVista($id_usuario, $id_pregunta);
 
         $this->view->render("partida", [
             'title' => 'Ruleta',
             'css' => '<link rel="stylesheet" href="/public/css/styles.css">',
             'usuario_id' => $id_usuario,
-            'pregunta' => $_SESSION['pregunta'],
-            'categoria' => $_SESSION['nombre_categoria'],
-            'respuestas' => $_SESSION['opciones'],
+            'pregunta' => $pregunta_texto,
+            'categoria' => $nombre_categoria,
+            'respuestas' => $respuestas,
             'id_partida' => $_SESSION['id_partida'],
             'tiempo_restante' => $tiempo_restante,
             'fondo' => $fondo,
@@ -47,11 +85,12 @@ class PartidaController
 
     }
 
-    public function responder(){
+    public function responder()
+    {
 
         $id_usuario = $_SESSION['usuario_id'] ?? null;
 
-        if($id_usuario == null){
+        if ($id_usuario == null) {
             header('Location: /inicio/show');
             exit;
         }
@@ -94,18 +133,15 @@ class PartidaController
                         $this->model->crearRegistroPreguntaRespondida($id_partida, $id_pregunta, $respuesta['id_respuesta'], 1);
                         $this->model->acumularPuntajeUsuario($id_usuario);
                         $this->model->sumarCorrectaAUsuario($id_usuario);
-                        $this->model->incrementoDeContestada($id_usuario);
-
                     }
                     $respuesta['clase'] = 'bg-success';
                 } elseif ($respuesta['id_respuesta'] == $_POST['id_respuesta']) {
                     $respuesta['clase'] = 'bg-danger';
-                        $texto = "¡INCORRECTA!";
-                        $color = 'text-danger';
+                    $texto = "¡INCORRECTA!";
+                    $color = 'text-danger';
 
                     $this->model->actualizarFechaPartidaFinalizada($_SESSION['id_partida']);
                     $this->model->crearRegistroPreguntaRespondida($id_partida, $id_pregunta, $respuesta['id_respuesta'], 0);
-                    $this->model->incrementoDeContestada($id_usuario);
                 } else {
 
                     $respuesta['clase'] = 'bg-light';
@@ -113,7 +149,7 @@ class PartidaController
                 $respuesta['disabled'] = true;
 
             }
-            
+
             $_SESSION['cantidad'] = intval($this->model->getCantidadDePreguntas($_SESSION['id_partida']));
 
             $fondo = $this->model->getColorCategoria($_SESSION['nombre_categoria']);
@@ -138,21 +174,8 @@ class PartidaController
                 'fondo' => $fondo,
                 'user' => $user
             ]);
-
-
-
-
         } else {
             echo 'error';
         }
-
-
-
-
     }
-
-
-
-
-
 }
