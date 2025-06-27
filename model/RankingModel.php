673d7fbfbd9793ea05f5353006bcedbbdad0d44e
Julieta <julibernacchia@gmail.com>
2025-06-27 20:33:43 -0300

    se excluyen los roles de editor y admin del ranking

--- model/RankingModel.php
+++ model/RankingModel.php
@@ -11,19 +11,23 @@ class RankingModel
 
     public function obtenerRanking()
     {
-        $sql = "SELECT id_usuario, nombre_usuario , foto_perfil_url, puntaje_acumulado
-        FROM usuarios
-        ORDER BY puntaje_acumulado DESC";
+        $sql = "SELECT u.id_usuario, u.nombre_usuario, u.foto_perfil_url, u.puntaje_acumulado
+            FROM usuarios u
+            JOIN usuario_rol ur ON u.id_usuario = ur.id_usuario
+            WHERE ur.id_rol = 1
+            ORDER BY u.puntaje_acumulado DESC";
         return $this->database->query($sql);
     }
 
     public function obtenerPartidasJugadas()
     {
         $sql = "SELECT p.id_partida, u.nombre_usuario, p.fecha_inicio, p.fecha_fin, p.puntaje_final, u.id_usuario
-        FROM partidas p
-        JOIN usuarios u ON p.id_usuario = u.id_usuario
-        ORDER BY p.puntaje_final DESC, p.fecha_fin DESC
-        LIMIT 10";
+            FROM partidas p
+            JOIN usuarios u ON p.id_usuario = u.id_usuario
+            JOIN usuario_rol ur ON u.id_usuario = ur.id_usuario
+            WHERE ur.id_rol = 1
+            ORDER BY p.puntaje_final DESC, p.fecha_fin DESC
+            LIMIT 10";
         return $this->database->query($sql);
     }
 
